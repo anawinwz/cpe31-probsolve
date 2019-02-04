ec0a3f1b49c11d2f8f6aecdf4659c4c249922594
Anawin Wongsadit <imonmac@hotmail.com>
2019-02-04 15:05:54 +0700

    P-PP----

--- week3/listwork1.cpp
+++ week3/listwork1.cpp
@@ -21,18 +21,27 @@ void List::insert_front(int data) {
   size++;
 }
 void List::delete_at(int k) {
-  if(size<k) return;
+  if(size < k) return;
+  //cout << "delete_at" << k << ","<<size<<endl;
   Node* p = header;
   Node* tmp;
   for(int i=1;i<k-1;i++) {
+    //cout << "next" << endl;
     p = p->next;
   }
-  tmp = p->next;
-  p->next = tmp->next;
+  if (size == 1) {
+    tmp = p;
+    header = NULL;
+  } else {
+    tmp = p->next;
+    p->next = tmp->next;
+  }
   size--;
   delete tmp;
 }
 void List::print_list() {
+  //if (size==0) return;
+
   Node* p = header;
   while (p!=NULL) {
     cout << p->data << endl;
@@ -50,6 +59,9 @@ int main() {
       case 'I':list.insert_front(inp);break;
       case 'D':list.delete_at(inp);break;
     }
+    //cout << "-------" << endl;
+    //list.print_list();
+    //cout << "-------" << endl;
   }
   list.print_list();
 
