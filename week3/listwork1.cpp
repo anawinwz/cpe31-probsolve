c71c6ab9806d78284072a16c7f870a2767dbe168
Anawin Wongsadit <imonmac@hotmail.com>
2019-02-04 14:29:04 +0700

    listwork1 Draft 1

---
+++ week3/listwork1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+struct Node {
+  int data;
+  Node* next = NULL;
+  Node(int data, Node* next)
+    : data(data), next(next) {}
+};
+class List {
+  private:
+    Node* header = NULL;
+  public:
+    int size = 0;
+    void insert_front(int data);
+    void delete_at(int k);
+    void print_list();
+};
+void List::insert_front(int data) {
+  Node* newNode = new Node(data, header);
+  header = newNode;
+  size++;
+}
+void List::delete_at(int k) {
+  if(size<k) return;
+  Node* p = header;
+  Node* tmp;
+  for(int i=1;i<k-1;i++) {
+    p = p->next;
+  }
+  tmp = p->next;
+  p->next = tmp->next;
+  size--;
+  delete tmp;
+}
+void List::print_list() {
+  Node* p = header;
+  while (p!=NULL) {
+    cout << p->data << endl;
+    p = p->next;
+  }
+}
+int main() {
+  List list;
+  int m;
+  cin >> m;
+  char cmd; int inp;
+  for (int i=0;i<m;i++) {
+    cin >> cmd >> inp;
+    switch(cmd) {
+      case 'I':list.insert_front(inp);break;
+      case 'D':list.delete_at(inp);break;
+    }
+  }
+  list.print_list();
+
+  return 0;
+}
