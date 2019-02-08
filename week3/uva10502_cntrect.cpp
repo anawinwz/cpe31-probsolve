04656cc9fc36707e4886be2397e5e23dfb9ce73c
Anawin Wongsadit <imonmac@hotmail.com>
2019-02-09 02:43:29 +0700

    uva10502_cntrect Accepted

--- week3/uva10502_cntrect.cpp
+++ week3/uva10502_cntrect.cpp
@@ -1,26 +1,53 @@
 #include<iostream>
 using namespace std;
-int countans(int n, int m,char tbl[101][101]) {
-  int ans = 0;
-  for(int i=0;i<n;i++) {
-    for(int j=0;j<m;j++) {
-      if(tbl[i][j]=='0') continue;
-      ans++;
-      if(j+1<m && tbl[i][j+1]=='1') ans++;
-      if(i+1<n && tbl[i+1][j]=='1') ans++; 
-    }
-  }
-  return ans;
+char tbl[101][101];
+int num[101][101], sum[101][101];
+void convert(int i, int j, char n) {
+  num[i][j] = n - '0';
+  sum[i][j] = num[i][j] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
 }
 int main(){
-  int n,m;
-  char tbl[101][101];
+  int n, m;
+  int tmp, ans=0;
   while (true) {
     cin >> n;
     if (n==0) break;
     cin >> m;
+    ans = 0;
     for(int i=0;i<n;i++) cin >> tbl[i];
-    cout << countans(n,m,tbl) << endl;
+    for(int i=0;i<=n;i++)
+      for(int j=0;j<=m;j++) {
+        sum[i][j] = 0;
+      }
+
+    for(int i=0;i<n;i++) {
+      for(int j=0;j<m;j++) {
+        convert(i+1,j+1,tbl[i][j]);
+      }
+    }
+    
+    /*cout << "+++++"<<endl;
+    for(int i=0;i<=n;i++) {
+      for(int j=0;j<=m;j++) cout << sum[i][j] << " ";
+      cout << endl;
+    }
+    cout << "-----"<<endl;*/
+    for(int h=1;h<=n;h++) {
+      for(int w=1;w<=m;w++) {
+        //cout << w <<"x" << h <<"\n";
+        for(int i=1;i+h-1<=n;i++) {
+          for(int j=1;j+w-1<=m;j++) {
+            //cout <<"\t("<<i<<","<<j<<")\t\t("<<i<<","<<j+w-1<<")"<<endl;
+            //cout <<"\t("<<i+h-1<<","<<j<<")\t\t("<<i+h-1<<","<<j+w-1<<")"<<endl;
+            tmp = sum[i+h-1][j+w-1] - sum[i+h-1][j-1] - sum[i-1][j+w-1] + sum[i-1][j-1];
+            //cout << "i"<<i<<", j"<<j<<" "<<tmp<<endl;
+            if (tmp==w*h) ans++;
+          }
+        }
+      }
+      //cout << endl;
+    }
+    cout << ans << endl;
   }
   return 0;
 }
