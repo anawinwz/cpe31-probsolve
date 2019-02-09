dd943e79cccf6f493e0093996be061a68fcb4875
Anawin Wongsadit <imonmac@hotmail.com>
2019-02-09 19:33:28 +0700

    chains -- try to last update

--- week3/chains.cpp
+++ week3/chains.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 struct Node {
   int data;
-  int isRev = 0;
+  int isRev;
+  int head;
+  int tail;
   Node* prev = NULL;
   Node* next = NULL;
-  int head = 0;
-  int tail = 0;
   Node(int data)
     : data(data) {}
 };
@@ -40,7 +40,8 @@ void tail_point(int mode, Node* header, Node* target) {
 }*/
 void print_list(Node* header) {
   Node* p = header;
-  int isRev = (p->isRev==1 || (p->next==NULL && p->prev!=NULL)) ? 1 : 0;
+  //int isRev = (p->isRev==1 || (p->next==NULL && p->prev!=NULL)) ? 1 : 0;
+  int isRev = (p->isRev==1 || p->data==p->tail) ? 1 : 0;
   while (p!=NULL) {
     if (isRev==0 && p->prev!=NULL) cout << " <- ";
     if (isRev==1 && p->next!=NULL) cout << " <- ";
@@ -64,10 +65,16 @@ void reverse_line(Node* header) {
   }
 }
 */
+void updateLast(Node*& curr, int*& lastRev, int*& lastHead, int*& lastTail) {
+  
+}
 int main() {
   Node* node[100002];
   for (int i=1;i<=100000;i++) node[i] = new Node(i);
   for (int i=1;i<=100000;i++) {
+    node[i]->head = 0;
+    node[i]->tail = 0;
+    node[i]->isRev = 0;
     if (i>1) node[i]->prev = node[i-1];
     if (i<100000) node[i]->next = node[i+1];
   }
@@ -103,19 +110,19 @@ int main() {
     print_list(node[1]);
     cout << endl << "--------" << endl;*/
     if(((curr->head!=0&&curr==node[curr->head])||(curr->tail!=0&&curr==node[curr->tail])) && curr->isRev!=lastRev) {
-      //cout <<"lastRev is changed to " << curr->isRev << endl;
+      cout <<"lastRev is changed to " << curr->isRev << endl;
       lastRev = curr->isRev;
     }
     if(curr->head!=0) {
       if(curr->head!=lastHead) {
         lastHead = curr->head;
-        //cout <<"lastHead is changed to "<<lastHead->data<<endl; 
+        cout <<"lastHead is changed to "<<lastHead<<endl; 
       }
     }
     if(curr->tail!=0) {
       if(curr->tail!=lastTail) {
         lastTail = curr->tail;
-        //cout <<"lastTail is changed to "<<lastTail->data<<endl; 
+        cout <<"lastTail is changed to "<<lastTail<<endl; 
       }
     }
     switch(cmd) {
@@ -135,29 +142,52 @@ int main() {
           curr = curr->next;
           tmpp = curr;
           curr = curr->prev;
+          cout << "curr is " <<curr->data<< ", tmpp is "<<tmpp->data<<endl;
 
-          //Last node before the unchained
-          //curr->isEnd = 1;
-          
-          //Unchained head node         
-          tmpp->prev = NULL;
+      
+          //curr- Last node before the unchained
+          //tmpp - Unchained head node  
+          curr->tail = curr->data;
+          node[curr->tail]->head = tmpp->data;
+          if(lastRev==0) {
+            tmpp->prev = NULL;
+          } else {
+            tmpp->next = NULL;
+          }
+          tmpp->head = tmpp->data;
+          tmpp->tail = lastTail;
+          tmpp->isRev = lastRev;
+          node[tmpp->tail]->head = tmpp->data;
+          node[tmpp->tail]->tail = lastTail;
+          node[tmpp->tail]->isRev = lastRev;
           //tmpp->isEnd = 1;
         } else if(lastRev == 1 && curr->prev!=NULL) {
           curr = curr->prev;
           tmpp = curr;
           curr = curr->next;
+          cout << "curr is " <<curr->data<< ", tmpp is "<<tmpp->data<<endl;
 
           //Last node before the unchained
           //curr->head = curr->data;
           //curr->isEnd = 1;
           
           //Unchained head node
-          tmpp->next = NULL;
+          if(lastRev==0) {
+            tmpp->prev = NULL;
+          } else {
+            tmpp->next = NULL;
+          }
+          tmpp->head = tmpp->data;
+          tmpp->tail = lastTail;
+          tmpp->isRev = lastRev;
+          node[tmpp->tail]->head = tmpp->data;
+          node[tmpp->tail]->tail = lastTail;
+          node[tmpp->tail]->isRev = lastRev;
           //tmpp->isEnd = 1;
         }
 
-        if (node[at]->isRev==1 || (node[at]->next==NULL && node[at]->prev!=NULL)) {
-          //cout << "this is reverse!" <<endl;
+        if (node[at]->tail==at) {
+          cout << "this is reverse!" <<endl;
           //reverse_line(node[at]);
           mode = 1;
           //print_list(node[at]);
@@ -173,43 +203,50 @@ int main() {
         print_list(node[at]);
         cout << endl << "++++++++" << endl;*/
         if (mode==0) {
-          if(node[at]->isRev==1) {
-            node[node[at]->head]->tail = node[at]->head;
+          //if(node[at]->isRev==1) {
+            /*node[node[at]->head]->tail = node[at]->head;
             node[node[at]->head]->head = at;
-            node[node[at]->head]->isRev = 0;
+            node[node[at]->head]->isRev = (node[at]->isRev==1)?0:1;
 
             tmp = node[at]->head;
             node[at]->head = at;
-            node[at]->tail = tmp;
-            /*cout << "Swapping head and tail to NORMAL!" << endl;
+            node[at]->tail = tmp;*/
+            node[at]->isRev = 0;
+            cout << "NORMAL!" << endl;
             print_list(node[at]);
-            cout << endl;*/
-          }
+            cout << endl;
+          //}
           node[at]->prev = curr;
           node[node[at]->tail]->next = NULL;
 
         } else {
           
-          if(node[at]->isRev==0) {
+          //if(node[at]->isRev==0) {
             //node[at] becomes head
             node[node[at]->head]->tail = node[at]->head;
             node[node[at]->head]->head = at;
-            node[node[at]->head]->isRev = 1;
+            node[node[at]->head]->isRev = (node[at]->isRev==1)?0:1;
 
             tmp = node[at]->head;
             node[at]->head = at;
             node[at]->tail = tmp;
-            /*cout << "Swapping head and tail to REVERSE!" << endl;
+            node[at]->isRev = (node[at]->isRev==1)?0:1;
+            cout << "Swapping head and tail to REVERSE!" << endl;
             print_list(node[at]);
-            cout << endl;*/
+            cout << endl;
+          //}
+          if(node[at]->isRev==1) { 
+            node[at]->next = curr;
+            node[node[at]->tail]->prev = NULL;
+          } else {
+            node[at]->prev = curr;
+            node[node[at]->tail]->next = NULL;
           }
-          node[at]->next = curr;
-          node[node[at]->tail]->prev = NULL;
         }
         
         
         //node[at]->isEnd = 0;
-        node[at]->isRev = mode;
+        //node[at]->isRev = mode;
         curr->isRev = lastRev;
 
         if(lastRev==0) curr->next = node[at]; 
