2d223a6b6330dd9b396d4fb5936949ab3143b828
Anawin Wongsadit <imonmac@hotmail.com>
2019-02-13 00:33:26 +0700

    lemmo ---------P--P-P----

--- week4/lemmo.cpp
+++ week4/lemmo.cpp
@@ -14,9 +14,7 @@ int ans1=0, ans2=0;
 int n, m;
 
 bool visited[MAXN+1][MAXN+1];
-bool isWorked[MAXN+1][MAXN+1];
-int afterClose[MAXN+1][MAXN+1];
-int result[2][MAXN+1][MAXN+1];
+pair<int,int> parent[MAXN+1][MAXN+1];
 
 bool bfs(int si, int sj, int drt, bool isCnt, char (&map)[MAXN+1][MAXN+1]);
 bool bfs(int sj, int sdrt) {
@@ -29,67 +27,28 @@ bool bfs(int si, int sj, int sdrt, bool isCnt, char (&map)[MAXN+1][MAXN+1]){
   //drt: 1-forward 0-backward
   int drt = sdrt;
   #ifdef debug
-  printf("--------- bfs(%d,%d,%d) ------------\n",si,sj,drt);
+  printf("--------- bfs(%d,%d,%d - %c) ------------\n",si,sj,drt,(drt)?'>':'<');
   #endif
-  if(result[sdrt][si][sj]==1) {
-    #ifdef debug
-      printf("Memorization at First\n");
-    #endif
-    return true;
-  }
-  bool ret = false;
+
   if(isCnt) {
     for(int i=0;i<n;i++) {
       for(int j=0;j<m;j++) {
         visited[i][j] = false;
-        isWorked[i][j] = false;
-        //layer[i][j] = -1;
+        parent[i][j] = make_pair(-1,-1);
       }
     }
   }
   
   //layer[si][sj] = 0;
+  parent[si][sj] = make_pair(si,sj);
   pair<int,int> v;
   list< pair< pair<int,int>, pair<int,int> > > Q;
   Q.push_back(make_pair(make_pair(si,sj),make_pair(-1,-1)));
-  int flrn = flrlist.size();
-  for(int flrnn=0;flrnn<flrn;flrnn++) {
-    v = flrlist[flrnn];
-    if((v.first==si&&((sdrt==1&&v.second<sj)||(sdrt==0&&v.second>sj))) || result[drt][v.first][v.second]==1) continue;
-    Q.push_back(make_pair(make_pair(si,sj),make_pair(v.first,v.second)));
-  }
   while(!Q.empty()) {
     pair< pair<int,int>, pair<int,int> > elm = Q.front();
     pair<int,int> u = elm.first;
     Q.pop_front();
 
-    /*
-    if(isCnt && !realVisited[u.first][u.second] && u.first<n-1 && map[u.first][u.second]=='#') {
-      //printf("not visited %d %d\n",u.first,u.second);
-      flrlist.push_back(make_pair(u.first, u.second));
-    }*/
-
-    //Check if this turn off works?
-    if(elm.second.first!=-1) {
-      if(isWorked[elm.second.first][elm.second.second]==true) continue;
-      else if(result[drt][u.first][u.second]==1) {
-        #ifdef debug
-        printf("Memorized after disabled %d %d! Now: %d\n",elm.second.first,elm.second.second,afterClose[elm.second.first][elm.second.second]+1);
-        #endif
-        isWorked[elm.second.first][elm.second.second] = true;
-        afterClose[elm.second.first][elm.second.second]++;
-        if(afterClose[elm.second.first][elm.second.second]>ans2) ans2 = afterClose[elm.second.first][elm.second.second];
-        continue;
-      }
-    } else {
-      if(result[drt][u.first][u.second]==1) {
-        #ifdef debug
-        printf("\tUse memorization at %d %d\n",u.first,u.second);
-        #endif
-        ret = true;
-        continue;
-      }
-    }
     #ifdef debug
     printf("now %d %d\t",u.first,u.second);
     if(elm.second.first!=-1) printf(" (skipped %d,%d)",elm.second.first, elm.second.second);
@@ -100,20 +59,11 @@ bool bfs(int si, int sj, int sdrt, bool isCnt, char (&map)[MAXN+1][MAXN+1]){
     vector< pair<int,int> > adj;
     switch(map[u.first][u.second]) {
       case '$': 
-        if(elm.second.first==-1) {
-          ret = true;
-          result[drt][u.first][u.second] = 1;
-        } else {
-          #ifdef debug
-          printf("Finally after disabled %d %d! Now: %d\n",elm.second.first,elm.second.second,afterClose[elm.second.first][elm.second.second]+1);
-          #endif
-          isWorked[elm.second.first][elm.second.second] = true;
-          afterClose[elm.second.first][elm.second.second]++;
-          if(afterClose[elm.second.first][elm.second.second]>ans2) ans2 = afterClose[elm.second.first][elm.second.second];
-        }
-        continue;
+        #ifdef debug
+        printf("Woohoo! Found treasure! %d %d\n",u.first,u.second);
+        #endif
+        return true;
       case '@':
-        if(result[drt][u.first][u.second]!=1) result[drt][u.first][u.second] = 2;
         continue;
       case '.':
         if(u.first+1<n) adj.push_back(make_pair(u.first+1, u.second));
@@ -151,21 +101,19 @@ bool bfs(int si, int sj, int sdrt, bool isCnt, char (&map)[MAXN+1][MAXN+1]){
         case '.':v.first++;break;
       }
         
-      //if(layer[v.first][v.second]==-1) {
-      if(v.first<n) {
-        //layer[v.first][v.second]=layer[u.first][u.second]+1;
-        Q.push_back(make_pair(make_pair(v.first, v.second), elm.second));
-        //if(elm.second.first==-1 && v.first+1<n && result[drt][v.first][v.second]!=1 && map[v.first][v.second]=='#')
-        //  Q.push_back(make_pair(make_pair(v.first+1, v.second), make_pair(v.first, v.second)));
-      } 
+      //if(parent[v.first][v.second].first==-1) {
+        parent[v.first][v.second].first = u.first;
+        parent[v.first][v.second].second = u.second;
+        if(v.first<n) {
+          Q.push_back(make_pair(make_pair(v.first, v.second), elm.second));
+        }
+      /*} else {
+        printf("Cannot go to %d %d, its parent: %d %d, current: %d %d\n",v.first,v.second,parent[v.first][v.second].first,parent[v.first][v.second].second,u.first,u.second);
+      }*/
     }
     
   }
-  if(ret) {
-    result[sdrt][si][sj] = 1;
-    ans1++;
-  }
-  return ret;
+  return false;
 }
 
 void read_input() {
@@ -177,52 +125,12 @@ void read_input() {
 
 int main() {
   read_input();
-  int rewardCnt = 0;
-  for(int i=0;i<n;i++) {
-    for(int j=0;j<m;j++) {
-      if(map[i][j]=='#') flrlist.push_back(make_pair(i,j));
-      if(map[n-1][j]=='$') rewardCnt++;
-    }
-  }
-  if(rewardCnt==0) {
-    printf("0 0");
-    return 0;
-  }
   for(int j=0;j<m;j++) {
-    bfs(j,0);
-    bfs(j,1);
+    if(bfs(j,0)) ans1++;
+    if(bfs(j,1)) ans1++;
   }
   
-  /*
-  int thisans = 0;
-  if(n==1) maxAns = ans;
-  else {
-    int flrn = flrlist.size(), i, j;
-    pair<int,int> flr;
-    //printf("flrn: %d\n",flrn);
-    for(int flrnn=0;flrnn<flrn;flrnn++) {
-      flr = flrlist[flrnn];
-      i = flr.first; j = flr.second;
-      //printf("flrnn %d, i j %d %d\n",flrnn,i,j);
-      map[i][j]='.';
-      thisans = 0;
-      for(int k=0;k<m;k++) {
-        if(bfs(k,'<',false,map)) thisans++;
-        if(bfs(k,'>',false,map)) thisans++;
-      }
-      
-      //printf("try to disable %d %d\told visitCnt: %d\n",i,j,visitCnt[i][j]);
-      
-     // printf("after disable %d %d: %d\n",i,j,thisans);
-      if(thisans>maxAns) maxAns = thisans;
-      map[i][j]='#';
-    }
-  }*/
   if(n==1) ans2 = ans1;
-  else {
-    //ans2 += ans1;
-    //ans2 = (ans2<=m*2)?ans2:m*2;
-  }
-  printf("%d %d",ans1,ans2);
+  printf("%d %d",ans1,ans1);
   return 0;
 }
