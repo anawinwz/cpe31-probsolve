1fd729d83365d625789482e71b541cde7d6b7860
Anawin Wongsadit <imonmac@hotmail.com>
2019-03-05 01:39:23 +0700

    inversion ----------

--- week7/inversion.cpp
+++ week7/inversion.cpp
@@ -1,5 +1,6 @@
 #include<cstdio>
 #include<cmath>
+//#define debug
 int a[100001];
 int n;
 long long ans = 0;
@@ -7,15 +8,18 @@ int* merge(int a[], int an, int b[], int bn) {
   int ai=0, bi=0, resi=0;
   int *res = new int[an+bn];
   while(ai<an) {
-    if(bi>=bn || a[ai]<=b[bi]) {
-      res[resi++]=a[ai++];
-    } else {
+    if(bi<bn && a[ai]>b[bi]) {
       ans++;
       res[resi++]=b[bi++];
-    }
+    } else  {
+      res[resi++]=a[ai++];
+    } 
   }
+  #ifdef debug
+  printf("\tbi: %d\n",bi);
+  #endif
   while(bi<bn) {
-    //ans++;
+    ans++;
     res[resi++]=b[bi++];
   }
   return res;
@@ -25,11 +29,27 @@ int* merge_sort(int a[], int an) {
   int split = ceil(an/2);
   return merge(merge_sort(a,split),split,merge_sort(a+split,an-split),an-split);
 }
-
+#ifdef debug
+long long bruteforce(){
+  long long ans = 0;
+  for(int i=0;i<n;i++) {
+    for(int j=i+1;j<n;j++) {
+      if(a[i]>a[j]) ans++;
+    }
+  }
+  return ans;
+}
+#endif
 int main() {
   scanf("%d",&n);
   for(int i=0;i<n;i++) scanf("%d",&a[i]);
-  merge_sort(a,n);
+  int* test = merge_sort(a,n);
   printf("%lli",ans);
+  #ifdef debug
+  printf("\n");
+  for(int i=0;i<n;i++) printf("%d ",test[i]);
+  printf("\n");
+  printf("\nBrute force: %lli",bruteforce());
+  #endif
   return 0;
 }
