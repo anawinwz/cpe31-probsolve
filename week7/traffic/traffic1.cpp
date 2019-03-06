5d17b37ce8436c7fd4f6ea725e9a23da5195f685
Anawin Wongsadit <imonmac@hotmail.com>
2019-03-06 11:01:35 +0700

    traffic1 [PPPPPPPPPP]

--- week7/traffic/traffic1.cpp
+++ week7/traffic/traffic1.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cmath>
 #include <cstdio>
+//#define debug
 using namespace std;
 
 int n,k;
@@ -17,9 +18,7 @@ int mindist(int x1, int y1, int x2, int y2) {
 main()
 {
   traffic_init(&n,&k);
-  int col = -1;
   int mid, lo, hi;
-  lo = 0; hi = n-1;
   for(int x=1;x<=n;x++) { // by col
     if(traffic_query(x,n,x,1)>n-1) {
       #ifdef debug
@@ -31,17 +30,12 @@ main()
         #ifdef debug
         cout << lo << " << " << mid << " >> " <<hi<<endl;
         #endif
-        if(abs(lo-mid)<=1 && abs(mid-hi)<=1) {
-          #ifdef debug
-          cout << lo << " "<<mid<<endl;
-          #endif
-          if(traffic_query(x,lo,x,mid)==2) {
-            traffic_report(x,lo,x,mid,0,0,0,0);
-          } else if(traffic_query(x,mid,x,hi)==2) {
-            traffic_report(x,mid,x,hi,0,0,0,0);
-          }
-        }
-        if(traffic_query(x,lo,x,mid) > abs(lo-mid)) {
+
+        if(traffic_query(x,mid-1,x,mid)==2) {
+          traffic_report(x,mid-1,x,mid,0,0,0,0);
+        } else if(traffic_query(x,mid,x,mid+1)==2) {
+          traffic_report(x,mid,x,mid+1,0,0,0,0);
+        } else if(traffic_query(x,lo,x,mid) > abs(lo-mid)) {
           hi = mid;
         } else {
           lo = mid+1;
@@ -61,17 +55,11 @@ main()
         #ifdef debug
         cout << lo << " << " << mid << " >> " <<hi<<endl;
         #endif
-        if(abs(lo-mid)<=1 && abs(mid-hi)<=1) {
-          #ifdef debug
-          cout << lo << " "<<mid<<endl;
-          #endif
-          if(traffic_query(lo,y,mid,y)==2) {
-            traffic_report(lo,y,mid,y,0,0,0,0);
-          } else if(traffic_query(mid,y,hi,y)==2) {
-            traffic_report(mid,y,hi,y,0,0,0,0);
-          }
-        }
-        if(traffic_query(lo,y,mid,y) > abs(lo-mid)) {
+        if(traffic_query(mid-1,y,mid,y)==2) {
+          traffic_report(mid-1,y,mid,y,0,0,0,0);
+        } else if(traffic_query(mid,y,mid+1,y)==2) {
+          traffic_report(mid,y,mid+1,y,0,0,0,0);
+        } else if(traffic_query(lo,y,mid,y) > abs(lo-mid)) {
           hi = mid;
         } else {
           lo = mid+1;
@@ -81,3 +69,4 @@ main()
     }
   }
 }
+ 
