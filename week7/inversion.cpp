ded0dd4c0d643277f8625a738ff5d4c497ffbe62
Anawin Wongsadit <imonmac@hotmail.com>
2019-03-10 17:30:07 +0700

    inversion PPPPPPPPPP

--- week7/inversion.cpp
+++ week7/inversion.cpp
@@ -4,20 +4,33 @@
 int a[100001];
 int n;
 long long ans = 0;
+
+int* merge2(int a[], int an, int b[], int bn) {
+  int ai=0, bi=0, resi=0;
+  int *res = new int[an+bn];
+  while(ai<an) {
+    if(bi>=bn || a[ai]>b[bi]) {
+      ans+=bn-bi;
+      res[resi++]=a[ai++];
+    } else {
+      res[resi++]=b[bi++];
+    }
+  }
+  while(bi<bn) {
+    res[resi++]=b[bi++];
+  }
+  return res;
+}
 int* merge(int a[], int an, int b[], int bn) {
   int ai=0, bi=0, resi=0;
   int *res = new int[an+bn];
   while(ai<an) {
-    if(bi<bn && a[ai]>b[bi]) {
-      ans++;
-      res[resi++]=b[bi++];
-    } else  {
+    if(bi>=bn || a[ai]<=b[bi]) {
       res[resi++]=a[ai++];
-    } 
+    } else {
+      res[resi++]=b[bi++];
+    }
   }
-  #ifdef debug
-  printf("\tbi: %d\n",bi);
-  #endif
   while(bi<bn) {
     res[resi++]=b[bi++];
   }
@@ -26,7 +39,7 @@ int* merge(int a[], int an, int b[], int bn) {
 int* merge_sort(int a[], int an) {
   if(an==1) return a;
   int split = ceil(an/2);
-  return merge(merge_sort(a,split),split,merge_sort(a+split,an-split),an-split);
+  return merge2(merge_sort(a,split),split,merge_sort(a+split,an-split),an-split);
 }
 #ifdef debug
 long long bruteforce(){
@@ -42,11 +55,9 @@ long long bruteforce(){
 int main() {
   scanf("%d",&n);
   for(int i=0;i<n;i++) scanf("%d",&a[i]);
-  int* test = merge_sort(a,n);
+  merge_sort(a, n);
   printf("%lli",ans);
   #ifdef debug
-  printf("\n");
-  for(int i=0;i<n;i++) printf("%d ",test[i]);
   printf("\n");
   printf("\nBrute force: %lli",bruteforce());
   #endif
