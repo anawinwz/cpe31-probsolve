e24cdd8b27c60e3f2b643b66f331236a59e8db91
Anawin Wongsadit <imonmac@hotmail.com>
2019-03-20 01:39:25 +0700

    mid_warehouses PPPPPPTTTTTT (with adj checks)

--- week8/mid_warehouses.cpp
+++ week8/mid_warehouses.cpp
@@ -7,23 +7,38 @@ vector<int> adj[MAXN+1];
 int n, m, k;
 bool bfs(int start, int end) {
   bool visited[MAXN+1] = {false};
+  int parent[MAXN+1];
+  bool isFound = false;
+
   list<int> Q;
+  int now;
   Q.push_back(start);
   while(!Q.empty()) {
-    int now = Q.front();
+    now = Q.front();
     Q.pop_front();
 
     visited[now] = true;
     if(now==end) {
-      return true;
+      //dp[start][end] = 2;
+      isFound = true;
+      break;
     }
 
     for(int i=0;i<adj[now].size();i++) {
       if(visited[adj[now][i]]) continue;
+      parent[adj[now][i]] = now;
       Q.push_back(adj[now][i]);
     }
   }
-  return false;
+  if(isFound) {
+    /*while(now!=0) {
+      dp[now][end] = 2;
+      now = parent[now];
+    } */
+  } else {
+    //dp[start][end] = 1;
+  }
+  return isFound;
 }
 int main() {
   scanf("%d %d %d",&n,&m,&k);
@@ -35,7 +50,7 @@ int main() {
   }
   for(int kk=0;kk<k;kk++){
     scanf("%d %d",&x,&y);
-    if(bfs(x,y)) printf("1");
+    if(adj[y].size()>0 && adj[x].size()>0 && bfs(x,y)) printf("1");
     else printf("0");
     printf("\n");
   }
