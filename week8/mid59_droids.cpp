1be74efa9bdafd4391cf5ee62577e2b6d85537c6
Anawin Wongsadit <imonmac@hotmail.com>
2019-03-20 10:41:57 +0700

    mid59_droids ---------------[--]-[---][---][----]

--- week8/mid59_droids.cpp
+++ week8/mid59_droids.cpp
@@ -1,33 +1,37 @@
 #include<cstdio>
 #include<algorithm>
-#include<cmath>
+#include<vector>
 using namespace std;
-int n, m, now=0, target=0;
-struct droid{
-  int idx;
-  int pos;
-};
-droid dr[100001];
-bool sortme(droid a, droid b) {
-  return (abs(target-a.pos) < abs(target-b.pos)) || (abs(target-a.pos)==abs(target-b.pos) && a.pos<b.pos);
-} 
+int n, m, now=0;
+vector<int> dr;
+
 int main() {
   scanf("%d %d",&n,&m);
+  int inp;
   for(int i=0;i<n;i++){
-    scanf("%d",&dr[i].pos);
-    dr[i].idx = i;
+    scanf("%d",&inp);
+    dr.push_back(inp);
   }
-  int ans=0;
+  sort(dr.begin(),dr.end());
+  int ans=0, target;
+  vector<int>::iterator res;
   for(int mm=0;mm<m;mm++) {
     scanf("%d",&target);
-    sort(dr,dr+n,sortme);
-    for(int i=0;i<n;i++) {
-      if(dr[i].pos!=now) {
-        ans+=abs(target-dr[i].pos);
-        now=dr[i].pos;
-        break;
+
+    if(target>dr[n-1]) {
+      res = dr.begin()+n-1;
+      if(*res==now) {
+        res--;
+      }
+    } else {
+      res = --upper_bound(dr.begin(),dr.end(),target);
+      if(*res==now) {
+        res = --upper_bound(res+1,dr.end(),target);
       }
     }
+    ans+=abs(target-(*res));
+    //printf("use %d for %d\n",*res,target);
+    now = *res;
   }
   printf("%d",ans);
   return 0;
