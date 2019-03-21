65eecd7b22d2dcfd3318042c508a7289a82047be
Anawin Wongsadit <imonmac@hotmail.com>
2019-03-21 13:03:40 +0700

    mid59_droids TTTT-TTTT-T-T-T[-T]T[T-T][T--][TT-T]

--- week8/mid59_droids.cpp
+++ week8/mid59_droids.cpp
@@ -3,41 +3,50 @@
 #include<vector>
 using namespace std;
 int n, m, now=0;
-vector<int> dr;
-
+int dr[100001], drI=0;
+int bsearch(int find) {
+  int lo=0,hi=n-1,mid;
+  while(true) {
+    mid=(lo+hi)/2;
+    //printf("%d %d %d\n",dr[lo],dr[mid],dr[hi]);
+    
+    if(dr[mid]<find && (mid+1>=n || find<dr[mid+1])) {
+      //printf("%d<%d && %d<%d",dr[mid],find,find,dr[mid+1]);
+      return mid;
+    }
+    
+    if(find==dr[mid]) {
+      return mid;
+    } else if(find<dr[mid]) {
+      hi=mid;
+    } else if(find>dr[mid]) {
+      lo=mid+1;
+    }
+  }
+}
 int main() {
   scanf("%d %d",&n,&m);
   int inp;
   for(int i=0;i<n;i++){
     scanf("%d",&inp);
-    dr.push_back(inp);
+    dr[drI++] = inp;
   }
-  sort(dr.begin(),dr.end());
-  int ans=0, target;
-  vector<int>::iterator res;
+  sort(dr,dr+n); 
+  int ans=0, target, i, tmp;
   for(int mm=0;mm<m;mm++) {
     scanf("%d",&target);
-
-    if(target>dr[n-1]) {
-      res = dr.begin()+n-1;
-      if(*res==now) {
-        res--;
-      }
-    } else if(target<dr[0]) {
-      res = dr.begin();
-      if(*res==now) {
-        res++;
-      }
-    } else {
-      res = lower_bound(dr.begin(),dr.end(),target);
-      if(*res!=target) --res;
-      if(*res==now) {
-        res = --lower_bound(res+1,dr.end(),target);
-      }
+    i = bsearch(target);
+    if(now!=dr[i]) {
+      ans += abs(target-dr[i]);
+      now = dr[i];
+      //printf("use %d for %d\n",dr[i],target);
+    }else {
+      if(i+1<n) tmp = i+1;
+      if(i+1>=n || (i-1>=0 && dr[i-1]<dr[tmp])) tmp = i-1;
+      ans += abs(target-dr[tmp]);
+      now = dr[tmp];
+      //printf("use %d for %d\n",dr[tmp],target);
     }
-    ans+=abs(target-(*res));
-    //printf("use %d for %d\n",*res,target);
-    now = *res;
   }
   printf("%d",ans);
   return 0;
