20554359809eff13d5a1ffbec2b869e615acc6ed
Anawin Wongsadit <imonmac@hotmail.com>
2019-03-21 10:34:01 +0700

    mid592_bread PPPPPPPP

--- week8/mid592_bread.cpp
+++ week8/mid592_bread.cpp
@@ -2,6 +2,7 @@
 #include<cmath>
 #include<algorithm>
 #include<vector>
+#include<map>
 using namespace std;
 struct shop {
   int pos;
@@ -9,28 +10,39 @@ struct shop {
   int amt;
 };
 int n, q;
-vector<shop> sh;
+//vector<shop> sh;
+map<int, shop> shidx;
 int home;
 bool sortme(shop a, shop b){
-  return a.price<b.price;
+  return a.price<b.price && abs(a.pos-home)<=5 && abs(b.pos-home) > 5; 
+}
+bool sortidx(int a,int b) {
+  return shidx[a].price<shidx[b].price; 
+}
+int min(int a,int b){
+  if(a<b) return a;
+  return b;
 }
 int main() {
   scanf("%d %d",&n,&q);
   for(int i=0;i<n;i++) {
     shop newsh;
     scanf("%d %d %d",&newsh.pos,&newsh.price,&newsh.amt);
-    sh.push_back(newsh);
+    if(newsh.amt!=0) {
+      //sh.push_back(newsh);
+      shidx[newsh.pos] = newsh;
+    }
   }
-  sort(sh.begin(),sh.end(),sortme);
+  //sort(sh.begin(),sh.end(),sortme);
   int cost;
   for(int qq=0;qq<q;qq++) {
     scanf("%d",&home);
     cost = 0;
+    /*sort(sh.begin(),sh.end(),sortme);
     for(vector<shop>::iterator it=sh.begin();it!=sh.end();) {
       shop& thisshop = *it;
-      if(thisshop.amt==0 || abs(thisshop.pos-home)>5) {
-        ++it;
-        continue;
+      if(abs(thisshop.pos-home)>5) {
+        break;
       }
       //printf("%d buy from %d(): %d\n",home,thisshop.pos,thisshop.price);
       thisshop.amt--;
@@ -38,6 +50,17 @@ int main() {
       if(thisshop.amt<=0) it = sh.erase(it);
       else ++it;
       break;
+    }*/
+    vector<int> cand;
+    for(int i=max(1,home-5);i<=min(home+5,1000000000);i++) {
+      if(shidx.count(i) && shidx[i].amt>0) {
+        cand.push_back(i);
+      }
+    }
+    if(!cand.empty()) {
+      sort(cand.begin(),cand.end(),sortidx);
+      cost = shidx[cand[0]].price;
+      shidx[cand[0]].amt--;
     }
     printf("%d\n",cost);
   }
