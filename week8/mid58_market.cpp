02bcaeadceeb85bbf78b9efc1c8f97491c8993b8
Anawin Wongsadit <imonmac@hotmail.com>
2019-03-28 11:21:49 +0700

    mid58_market PPPPPPPPPPPP (inspired by koaliesz)

--- week8/mid58_market.cpp
+++ week8/mid58_market.cpp
@@ -15,7 +15,7 @@ int main() {
     switch(type) {
       case 1: //Buyer
         while(sell.begin()!=sell.end() && sell.begin()->first<=limit && amt > 0) {
-          if(sell.begin()->second > amt) {
+          if(sell.begin()->second > amt) { //stock > buy
             mval += limit * amt;
            // printf("buy %dea price %d\n",amt,limit);
             sell.begin()->second -= amt;
@@ -35,8 +35,11 @@ int main() {
         }
         break;
       case 2: //Seller
-        for(map<int,int>::iterator it=buy.lower_bound(limit); it!=buy.end();) {
-          if(it->first < limit || amt <= 0) break;
+        for(map<int,int>::reverse_iterator it=buy.rbegin(); amt > 0 && it!=buy.rend() && it->first >= limit; ) {
+          if(it->second <= 0) {
+            ++it;
+            continue;
+          }
 
           if(amt < it->second) {
             mval += limit * amt;
@@ -48,8 +51,8 @@ int main() {
             mval += limit * it->second;
           //  printf("sell %dea price %d\n",it->second,limit);
             amt -= it->second;
-            
-            buy.erase(it++);
+            it->second = 0;
+            //buy.erase(it++);
           }
         }
         if(amt > 0) {
