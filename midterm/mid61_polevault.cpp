064b48b1a3e6f316447140cf00ee1f6d15cf753b
Anawin Wongsadit <imonmac@hotmail.com>
2019-04-01 13:55:23 +0700

    mid61_polevault -------------------- (New ver)

--- midterm/mid61_polevault.cpp
+++ midterm/mid61_polevault.cpp
@@ -1,29 +1,42 @@
 #include<cstdio>
-#include<set>
+#include<vector>
+#include<algorithm>
+//#define debug
 using namespace std;
-int n, q;
-int pole[1001];
-set<int> alen;
-int connect(int i) {
-  int tmp = pole[i];
-  alen.insert(pole[i]);
-  if(i+1 < n) {
-    tmp += connect(i+1);
-    alen.insert(tmp);
-  }
-  return tmp;
-}
+vector<long long int> sum;
+int n,q;
 int main() {
+  long long int inp;
+  sum.push_back(0);
   scanf("%d %d",&n,&q);
   for(int i=0;i<n;i++) {
-    scanf("%d",&pole[i]);
+    scanf("%lli",&inp);
+    sum.push_back(sum[i]+inp);
   }
-  connect(0);
-  int inp;
+  vector<long long int>::iterator it;
+  long long int tmp;
   for(int i=0;i<q;i++) {
-    scanf("%d",&inp);
-    if(alen.count(inp)!=0) printf("Y");
-    else printf("N");
+    scanf("%lli",&inp);
+    it = lower_bound(sum.begin(),sum.end(),inp);
+    if(it==sum.end()) {
+      printf("N");
+      continue;
+    }
+    
+    if(*it==inp) {
+      printf("Y");
+    } else {
+      tmp = *it;
+      #ifdef debug
+      printf("\t%d(question) vs %d(in sum) to find %d\n",inp,tmp,tmp-inp);
+      #endif
+      it = find(sum.begin(),sum.end(),tmp-inp);
+      if(it!=sum.end()) printf("Y");
+      else printf("N");
+    }
+    #ifdef debug
+    printf("\n");
+    #endif
   }
   return 0;
 }
