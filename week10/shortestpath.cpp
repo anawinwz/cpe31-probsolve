08fdd00b83c316302a8c185768ca639914e62524
Anawin Wongsadit <imonmac@hotmail.com>
2019-04-27 00:08:05 +0700

    shortestpath PPPPPP-P

--- week10/shortestpath.cpp
+++ week10/shortestpath.cpp
@@ -3,41 +3,41 @@
 #include<vector>
 #include<climits>
 #include<utility>
-#include<algorithm>
 #define MAXN 100001
 using namespace std;
+typedef pair<int, int> intpair;
 long long int dist[MAXN];
-//bool visited[MAXN];
-set<pair<int, int> > adj[MAXN];
-vector<int> vertex; 
+bool visited[MAXN];
+set<intpair> waiting;
+vector<intpair> adj[MAXN]; 
 int n, m, visitCnt = 0;
-bool sortme(int a, int b) {
-  return dist[a] < dist[b];
-}
 void dijkstra() {
   int bestu;
-  while(visitCnt < n) {
-    sort(vertex.begin(),vertex.end(),sortme);
-    bestu = vertex.front();
-    vertex.erase(vertex.begin());
-    visitCnt++;
-    for(set<pair<int, int> >::iterator it=adj[bestu].begin();it!=adj[bestu].end();++it) {
+  for(int i=0;i<n;i++){
+    bestu = waiting.begin()->second;
+    waiting.erase(waiting.begin());
+    
+    for(vector<intpair>::iterator it=adj[bestu].begin();it!=adj[bestu].end();++it) {
+      if(visited[bestu]) continue;
       if(dist[bestu]+it->first < dist[it->second]) {
         dist[it->second] = dist[bestu]+it->first;
+        waiting.insert(make_pair(dist[it->second], it->second));
       }
     }
+    visited[bestu] = true;
   }
 }
 int main() {
   scanf("%d %d",&n,&m);
-  for(int i=1;i<=n;i++) dist[i] = INT_MAX, vertex.push_back(i);
+  for(int i=1;i<=n;i++) dist[i] = INT_MAX;
   int a,b,w;
   dist[1] = 0;
   for(int mm=0;mm<m;mm++) {
     scanf("%d %d %d",&a,&b,&w);
-    adj[a].insert(make_pair(w,b));
-    adj[b].insert(make_pair(w,a));
+    adj[a].push_back(make_pair(w,b));
+    adj[b].push_back(make_pair(w,a));
   }
+  waiting.insert(make_pair(0, 1));
   dijkstra();
   printf("%lli",dist[n]);
   return 0;
